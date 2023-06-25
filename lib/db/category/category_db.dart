4eb5196fd0d8c509c100b9e5917a2c219a8f1b96
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-25 12:58:37 +0530

    db

---
+++ lib/db/category/category_db.dart
@@ -0,0 +1,27 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:money_manager/models/category/category_model.dart';
+
+const CATEGORY_DB_NAME = 'category-database';
+
+abstract class CategoryDbFunctions {
+  Future<List<CategoryModel>> getCategories();
+  Future<void> insertCategory(CategoryModel category);
+}
+
+class CategoryDB implements CategoryDbFunctions {
+
+  
+
+
+  @override
+  Future<void> insertCategory(CategoryModel value) async {
+    final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
+    await _categoryDB.add(value);
+  }
+
+  @override
+  Future<List<CategoryModel>> getCategories() async {
+    final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
+    return _categoryDB.values.toList();
+  }
+}
